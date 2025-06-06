eb09c446db197c33e31e2cc642b500156de755d8
williampss <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>
2025-06-06 00:23:48 -0300

    att_readingSite

--- src/js/manhwa.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      freeMode: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
